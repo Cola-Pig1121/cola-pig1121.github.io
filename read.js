8c302fb1498e2f711e4e6da979776a2a78df5e16
信奥图床 <123631723+Cola-Pig1121@users.noreply.github.com>
2024-07-23 21:22:24 +0800

    Update read.js

--- read.js
+++ read.js
@@ -1,24 +1,43 @@
+// node fs模块
 const fs = require('fs');
-
-function 递归遍历文件夹(folderPath) {
-	fs.readdir(folderPath, (err, files) => {
-		if (err) throw err;
-
-		files.forEach(file => {
-			const currentPath = `${folderPath}/${file}`;
-			fs lstat(currentPath, (err, stats) => {
-				if (err) throw err;
-
-				if (stats.isDirectory()) {
-					// 如果是目录，则继续递归
-					递归遍历文件夹(currentPath);
-				} else {
-					// 如果是文件，则打印文件路径
-          document.getElementById("img").innerHTML = currentPath；
+// node path模块
+const path = require('path');
+// 收集所有的文件路径
+const arr = [];
+let timer = null;
+const fileDisplay = (url, cb) => {
+	const filePath = path.resolve(url);
+	//根据文件路径读取文件，返回文件列表
+	fs.readdir(filePath, (err, files) => {
+		if (err) return console.error('Error:(spec)', err)
+		files.forEach((filename) => {
+			//获取当前文件的绝对路径
+			const filedir = path.join(filePath, filename);
+			// fs.stat(path)执行后，会将stats类的实例返回给其回调函数。
+			fs.stat(filedir, (eror, stats) => {
+				if (eror) return console.error('Error:(spec)', err);
+				// 是否是文件
+				const isFile = stats.isFile();
+				// 是否是文件夹
+				const isDir = stats.isDirectory();
+				if (isFile) {
+					// 这块我自己处理了多余的绝对路径，第一个 replace 是替换掉那个路径，第二个是所有满足\\的直接替换掉
+					arr.push(filedir.replace(__dirname, '').replace(/\\/img, '/'))
+					// 最后打印的就是完整的文件路径了
+					if (timer) clearTimeout(timer)
+					timer = setTimeout(() => cb && cb(arr), 200)
 				}
-			});
+				// 如果是文件夹
+				if (isDir) fileDisplay(filedir, cb);
+			})
 		});
 	});
 }
-
-递归遍历文件夹(./image);
+// 测试代码
+fileDisplay('./image', (arr) => {
+	for(let i = 0; i< arr.length; i++)
+			console.log(arr[i]);
+			document.write('<a href="', arr[i], '">', arr[i], '</a>');
+})
+// commonjs规范
+module.exports = fileDisplay;
