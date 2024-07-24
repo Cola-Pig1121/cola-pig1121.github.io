1acf914f2299fe443a76a6ef0150d5d06e863a1d
信奥图床 <123631723+Cola-Pig1121@users.noreply.github.com>
2024-07-24 14:42:31 +0800

    Update read.js

--- read.js
+++ read.js
@@ -53,7 +53,7 @@ const injectToHtml = (ar) => {
 	data.splice(
 		data.findIndex((c) => c.trim() === "<footer>"),
 		null,
-		` <br> <div> <span class="pre" ><a href=".`,ar,`">`,ar,`</a></span> <img src=".`,ar,`" width="25%" height="25%" > </div> `
+		` <a href=".`,ar,`"><img src=".`,ar,`" width="25%" height="25%" > </a> `
 	);
 	fs.writeFileSync(html, data.join(EOL));
 };
