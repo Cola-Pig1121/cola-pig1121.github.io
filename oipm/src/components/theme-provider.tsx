16841e04ce5e225d32071773329b571cc9c8fd98
Cola Pig <123631723+Cola-Pig1121@users.noreply.github.com>
2025-08-12 16:56:37 +0800

    Delete oipm directory

--- oipm/src/components/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
